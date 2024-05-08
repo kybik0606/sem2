a2bf449504312a6e23f9f8a1e8bf6855fde57c60
Aleksei <87711803+kybik0606@users.noreply.github.com>
2024-05-08 14:34:37 +0300

    upload 4.2

---
+++ 4.2.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include "Students.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Determine_underachieving_students(const vector<Student>& students, unsigned int course, const string& subject);
+
+int main() {
+    vector<Student> students;
+    int num_students;
+    cout << "Enter the number of students: ";
+    cin >> num_students;
+
+    for (int i = 0; i < num_students; ++i) {
+        Student student;
+        student.fill_data_of_student();
+        students.push_back(student);
+        student.save_binaryfile();
+    }
+
+    unsigned int course;
+    string subject;
+    cout << "Enter the course number: ";
+    cin >> course;
+    cout << "Enter the subject: ";
+    cin >> subject;
+
+    Determine_underachieving_students(students, course, subject);
+
+    return 0;
+}
+void Determine_underachieving_students(const vector<Student>& students, unsigned int course, const string& subject) {
+    int count = 0;
+    for (const auto& student : students) {
+        if (student.getCourse() == course && student.getSubject() == subject && student.getGrades() < 4) {
+            ++count;
+        }
+    }
+    cout << "Number of underachieving students " << course << " for subject " << subject << ": " << count << endl;
+}
---
+++ Students.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+class Student {
+    string name;
+    unsigned int age;
+    unsigned int course;
+    unsigned int gender;
+    double grades;
+    string subject;
+
+public:
+    Student();
+    Student(const string& name, unsigned int age, unsigned int course, unsigned int gender, double grades, const string& subject);
+    Student(const Student& other);
+    Student(Student&& other);
+    ~Student();
+
+    void fill_data_of_student();
+    void load_binaryfile();
+    void save_binaryfile();
+    void output_to_the_screen();
+
+    string getName() const;
+    unsigned int getAge() const;
+    unsigned int getCourse() const;
+    unsigned int getGender() const;
+    double getGrades() const;
+    string getSubject() const;
+};
