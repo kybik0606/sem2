d21061b19973237af892dc4d227490d74d9b978a
Aleksei <87711803+kybik0606@users.noreply.github.com>
2024-05-14 10:47:33 +0300

    Update 4.2.cpp

--- 4.2.cpp
+++ 4.2.cpp
@@ -7,34 +7,35 @@ using namespace std;
 
 void Determine_underachieving_students(const vector<Student>& students, unsigned int course, const string& subject);
 
-int main() {
+int main() 
+{
     vector<Student> students;
     int num_students;
     cout << "Enter the number of students: ";
     cin >> num_students;
-
-    for (int i = 0; i < num_students; ++i) {
+    for (int i = 0; i < num_students; ++i) 
+    {
         Student student;
         student.fill_data_of_student();
         students.push_back(student);
         student.save_binaryfile();
     }
-
     unsigned int course;
     string subject;
     cout << "Enter the course number: ";
     cin >> course;
     cout << "Enter the subject: ";
     cin >> subject;
-
     Determine_underachieving_students(students, course, subject);
-
     return 0;
 }
-void Determine_underachieving_students(const vector<Student>& students, unsigned int course, const string& subject) {
+void Determine_underachieving_students(const vector<Student>& students, unsigned int course, const string& subject) 
+{
     int count = 0;
-    for (const auto& student : students) {
-        if (student.getCourse() == course && student.getSubject() == subject && student.getGrades() < 4) {
+    for (const auto& student : students)
+    {
+        if (student.get_course() == course && student.get_subject() == subject && student.get_grades() < 4) 
+        {
             ++count;
         }
     }
