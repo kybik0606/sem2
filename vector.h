664a81a10c5eb19d66bd148001c06b222e9f99fd
Aleksei <87711803+kybik0606@users.noreply.github.com>
2024-05-28 00:29:00 +0300

    Update vector.h(class Vector)

--- vector.h
+++ vector.h
@@ -5,28 +5,33 @@
 #include <ctime>
 #include <algorithm>
 
-class vector {
+class Vector 
+{
 private:
     int size;
     int* vctr;
 
 public:
-    vector();
-    vector(int size);
-    vector(const vector& other);
-    vector(const vector&& other);
-    ~vector();
+    Vector();
+    Vector(int size);
+    Vector(const Vector& other);
+    Vector(const Vector&& other);
+    ~Vector();
 
     void set_vector();
     int get_size();
     int get_element(int index);
     void set_element(int index, int number);
-    void actions_with_scalar();
+    void add_scalar(int scalar);
+    void subtract_scalar(int scalar);
+    void multiply_scalar(int scalar);
+    void divide_scalar(int scalar);
+    void sort_vector();
     void find_scalar_lenght();
     void get_all_elements();
     void set_all_elements();
     void random_elements_of_vector();
-    void task(vector& vctr1);
-    int scalar_proizv(vector& vctr1, vector& vctr2);
-    vector& operator=(const vector& vctr1);
+    void task(Vector& vctr1);
+    int scalar_proizv(Vector& vctr1, Vector& vctr2);
+    Vector& operator=(const Vector& vctr1);
 };
