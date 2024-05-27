55b7f66baa1ee4e1607660edd5a029feff0df576
Aleksei <87711803+kybik0606@users.noreply.github.com>
2024-05-27 23:24:11 +0300

    UPLOAD UnattendedBag

--- UnattendedBag.h
+++ UnattendedBag.h
@@ -1,24 +1,42 @@
 #pragma once
 
-class UnattendedBag : public Entity {
+#include <stdexcept>
+
+class UnattendedBag : public Entity
+{
 private:
     int firstSeenTime;
 
 public:
     UnattendedBag(int xMin, int yMin, int xMax, int yMax, int timeSeen)
-        : Entity(xMin, yMin, xMax, yMax), firstSeenTime(timeSeen) {
-        if (timeSeen < 0 || timeSeen >= 86400) { 
-            throw std::invalid_argument("Invalid time of day");
-        }
+        : Entity(xMin, yMin, xMax, yMax)
+    {
+        setFirstSeenTime(timeSeen);
     }
 
-    void display() const override {
+    void setFirstSeenTime(int timeSeen)
+    {
+        if (timeSeen < 0 || timeSeen >= 86400)
+        {
+            throw std::invalid_argument("Invalid time of day");
+        }
+        firstSeenTime = timeSeen;
+    }
+
+    int getFirstSeenTime() const
+    { 
+        return firstSeenTime;
+    }
+
+    void display() const override
+    {
         Entity::display();
         std::cout << "Type: Unattended Bag\n";
         std::cout << "First Seen Time: " << firstSeenTime << " seconds after midnight\n";
     }
 
-    std::string getType() const override {
+    std::string getType() const override 
+    {
         return "Unattended Bag";
     }
 };
