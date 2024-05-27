f5c19ee5cbfdd12f6dfa52c5c752906fa96b870d
Aleksei <87711803+kybik0606@users.noreply.github.com>
2024-05-27 23:23:26 +0300

    UPLOAD Pedestrian

--- Pedestrian.h
+++ Pedestrian.h
@@ -3,7 +3,8 @@
 enum class Gender { Male, Female, Undefined };
 enum class BeardStatus { HasBeard, NoBeard, Unknown };
 
-class Pedestrian : public Entity {
+class Pedestrian : public Entity
+{
 private:
     Gender gender;
     BeardStatus beardStatus;
@@ -12,14 +13,34 @@ public:
     Pedestrian(int xMin, int yMin, int xMax, int yMax, Gender g, BeardStatus b)
         : Entity(xMin, yMin, xMax, yMax), gender(g), beardStatus(b) {}
 
-    void display() const override {
+    void setGender(Gender g) 
+    {
+        gender = g;
+    }
+    Gender getGender() const 
+    { 
+        return gender; 
+    }
+
+    void setBeardStatus(BeardStatus b)
+    { 
+        beardStatus = b; 
+    }
+    BeardStatus getBeardStatus() const 
+    { 
+        return beardStatus;
+    }
+
+    void display() const override
+    {
         Entity::display();
         std::cout << "Type: Pedestrian\n";
         std::cout << "Gender: " << (gender == Gender::Male ? "Male" : (gender == Gender::Female ? "Female" : "Undefined")) << "\n";
         std::cout << "Beard: " << (beardStatus == BeardStatus::HasBeard ? "Has Beard" : (beardStatus == BeardStatus::NoBeard ? "No Beard" : "Unknown")) << "\n";
     }
 
-    std::string getType() const override {
+    std::string getType() const override 
+    {
         return "Pedestrian";
     }
 };
